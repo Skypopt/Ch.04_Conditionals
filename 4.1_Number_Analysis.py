c8b780123fc3b4313ea9977c40315472a0e56b41
thompsonlan <llthompson1207@gmail.com>
2021-09-22 08:12:19 -0500

    finished 4.2 Grading

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,18 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+number=float(input("Enter a number"))
+if (number % 2) == 0:
+    print("Test 1: number is even")
+else:
+    print("Test 1: number is odd")
+if number > 0:
+    print("Test 2: number is positive")
+elif number == 0:
+    print("Test 2: number is 0")
+else:
+    print("Test 2: number is negative")
+if number > -100 and number < 100:
+    print("Test 3: Inclusive")
+else:
+    print("Test 3: Exclusive")
--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -4,3 +4,20 @@ GRADING 2.0
 Copy your Grading 1.0 program and modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
+semgrade=float(input("what is the semester grade? "))
+finalexam=float(input("what is the final exam grade? "))
+examworth=float(input("what is the exam worth? "))
+leftoverweight=1-examworth
+overall= (semgrade*leftoverweight)+(finalexam*examworth)
+print(overall)
+if overall >= 0.90:
+    print("Final Grade: -A / A")
+if overall >= 0.80 and overall < 0.90:
+    print("Final Grade: -B / B / B+")
+if overall >= 0.70 and overall <0.80:
+    print("Final Grade: -C / C / C+")
+if overall >=0.60 and overall <0.70:
+    print("Final Grade: -D / D / D+")
+if overall <0.60:
+    print("Final Grade: F")
+    print("Transfer to Johnston!")
